e34e361dc6ce7dd75e81a3f938e2f6cbd3cef350
ManarShahin48 <manarshahin48@gmail.com>
2023-02-21 12:06:13 +0200

    Working without HEROS

--- lib/main.dart
+++ lib/main.dart
@@ -2,27 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:ui_exercise/const.dart';
 import 'package:ui_exercise/home_view.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Constants.createMaterialColor(
           const Color(0xFFE00800),
         ),
         primaryColor: const Color(0xFFE00800),
       ),
-      initialRoute: '/first',
-      routes: {
-        '/first': (context) => const FirstScreen(),
-        '/second': (context) => const SecondScreen(),
-        '/third': (context) => const ThirdScreen(),
-      },
+      home: const HomeView(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
--- lib/main_con.dart
+++ lib/main_con.dart
@@ -12,9 +12,7 @@ class MainContainer extends StatelessWidget {
     this.width = 100,
     this.height = 100,
     this.color = Colors.black,
-    this.fontSize1 = 18,
-    this.fontSize2 = 18,
-    required this.heroTag,
+    this.fontSize = 15,
   }) : super(key: key);
 
   final String img;
@@ -22,77 +20,64 @@ class MainContainer extends StatelessWidget {
   final String? txt2;
   final double? width;
   final double? height;
-  final double fontSize1;
-  final double fontSize2;
+  final double fontSize;
   final int index;
   final Color color;
-  final int heroTag;
   final void Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Hero(
-      tag: 'Hero$heroTag',
-      child: Material(
-        elevation: 0,
-        color: Colors.transparent,
-        child: InkWell(
-          onTap: onTap,
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(color: color),
-                    borderRadius: const BorderRadius.all(
-                      Radius.circular(25.0),
-                    ),
-                    boxShadow: [
-                      BoxShadow(
-                          blurRadius: 10,
-                          color: color,
-                          offset: const Offset(1, 3))
-                    ],
-                    color: const Color(0xFF000000),
-                  ),
-                  width: width,
-                  height: height,
-                  margin: const EdgeInsets.all(15.0),
-                  padding: const EdgeInsets.all(30.0),
-                  child: SvgPicture.asset(img),
-                ),
-                Text(
-                  txt1,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: fontSize1,
-                    shadows: const <Shadow>[
-                      Shadow(
-                        offset: Offset(1.0, 1.0),
-                        blurRadius: 3.0,
-                        color: Colors.redAccent,
-                      ),
-                    ],
-                  ),
-                ),
-                Text(
-                  txt2!,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: fontSize2,
-                    shadows: const <Shadow>[
-                      Shadow(
-                        offset: Offset(1.0, 1.0),
-                        blurRadius: 3.0,
-                        color: Colors.redAccent,
-                      ),
-                    ],
-                  ),
+    return InkWell(
+      onTap: onTap,
+      child: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              border: Border.all(color: color),
+              borderRadius: const BorderRadius.all(
+                Radius.circular(25.0),
+              ),
+              boxShadow: [
+                BoxShadow(
+                    blurRadius: 10, color: color, offset: const Offset(1, 3))
+              ],
+              color: const Color(0xFF000000),
+            ),
+            width: width,
+            height: height,
+            margin: const EdgeInsets.all(15.0),
+            padding: const EdgeInsets.all(30.0),
+            child: SvgPicture.asset(img),
+          ),
+          Text(
+            txt1,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: fontSize,
+              shadows: const <Shadow>[
+                Shadow(
+                  offset: Offset(1.0, 1.0),
+                  blurRadius: 3.0,
+                  color: Colors.redAccent,
                 ),
               ],
             ),
           ),
-        ),
+          Text(
+            txt2!,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: fontSize,
+              shadows: const <Shadow>[
+                Shadow(
+                  offset: Offset(1.0, 1.0),
+                  blurRadius: 3.0,
+                  color: Colors.redAccent,
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
